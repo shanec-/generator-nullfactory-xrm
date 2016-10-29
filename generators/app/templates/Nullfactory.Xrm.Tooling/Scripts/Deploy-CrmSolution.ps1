ff96170d36c735c143bc8bf3d64427007b791ab4
Shane Carvalho <shanec_@hotmail.com>
2016-10-30 09:22:40 +1100

    Added support to both online and onprem deployment

--- generators/app/templates/Nullfactory.Xrm.Tooling/Scripts/Deploy-CrmSolution.ps1
+++ generators/app/templates/Nullfactory.Xrm.Tooling/Scripts/Deploy-CrmSolution.ps1
@@ -8,6 +8,8 @@ param(
 	[Parameter(Mandatory=$true, Position=4)]
 	[string]$solutionName,
 	[Parameter(Mandatory=$false)]
+	[switch]$isOnPremServer,
+	[Parameter(Mandatory=$false)]
     [switch]$deployManagedSolution,
     [Parameter(Mandatory=$false)]
     [switch]$activatePlugIns,
@@ -15,19 +17,30 @@ param(
 	[switch]$publishChanges
 )
 
-Write-Verbose "Import Micrsoft.Xrm.Data.Powershell module"
-Import-Module Microsoft.Xrm.Data.Powershell
+Write-Verbose "Intializing Micrsoft.Xrm.Data.Powershell module"
+Install-Module -Name Microsoft.Xrm.Data.PowerShell -Scope CurrentUser -ErrorAction SilentlyContinue -Force
 
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $creds = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
 
-Write-Verbose "Connecting to the crm instance $serverUrl ..."
-Connect-CrmOnPremDiscovery -Credential $creds -ServerUrl $serverUrl
+if($isOnPremServer)
+{
+	Write-Verbose "Connecting to the OnPrem crm instance $serverUrl ..."
+	Connect-CrmOnPremDiscovery -Credential $creds -ServerUrl $serverUrl
+}
+else
+{
+	Write-Verbose "Connecting to the Online crm instance $serverUrl ..."
+	Connect-CrmOnline -Credential $creds -ServerUrl $serverUrl
+}
 
 $releaseZipFileName = "";
 
-if (-Not $deployManagedSolution){ $releaseZipFileName = Resolve-Path(".\$solutionName.zip") } 
-else { $releaseZipFileName = Resolve-Path(".\$solutionName_managed.zip") }
+if (-Not $deployManagedSolution){ $releaseZipFileName = Resolve-Path("..\..\$solutionName\*\$solutionName.zip") } 
+else { $releaseZipFileName = Resolve-Path("..\..\$solutionName\*\$solutionName_managed.zip") }
 
-Write-Verbose "Importing the $releaseZipFileName solution..."
-Import-CrmSolution -SolutionFilePath $releaseZipFileName -PublishChanges $publishChanges -ActivatePlugIns $activatePlugIns
+Write-Verbose "Importing the $releaseZipFileName solution into $serverUrl ..."
+Import-CrmSolution 
+	-SolutionFilePath $releaseZipFileName `
+	-PublishChanges $publishChanges `
+	-ActivatePlugIns $activatePlugIns
