5f7670067539dcf5e998b3b6b03ec6ee1063bbe4
Shane Carvalho <shanec_@hotmail.com>
2017-12-28 16:13:56 +1100

    Added the ability to generate YAML CI Builds - Initial implementation of #30

---
+++ generators/cibuild/index.js
@@ -0,0 +1,61 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to ' + chalk.red('nullfactory-xrm') + '\n The Dynamics CRM/365 Project Structure Generator!'
+    ));
+
+    var prompts = [
+    {
+      type: 'input',
+      name: 'visualStudioSolutionProjectPrefix',
+      message: 'Visual Studio solution project filename prefix?',
+      default: 'Nullfactory'
+    },
+    {
+      type: 'list',
+      name: 'project',
+      message: 'What type of project do you want to create today?',
+          choices: [
+              "Visual Studio Team Services",
+          ]
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+
+    this._writeVsts();
+  },
+
+  // vs solution
+  _writeVsts: function () {
+    this.fs.copyTpl(
+      this.templatePath('vsts.y_l'),
+      this.destinationPath('.vsts-ci.yml'), {
+        visualStudioSolutionProjectPrefix: this.props.visualStudioSolutionProjectPrefix,
+      }
+    );
+  },
+
+  install: function () {
+    // this.installDependencies();
+  },
+
+  end: function () {
+    var postInstallSteps =
+      chalk.green.bold('\nSuccessfully generated CI build yml.');
+
+    this.log(postInstallSteps);
+  }
+});
+///
